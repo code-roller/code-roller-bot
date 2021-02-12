578b3d28ab325edacd299c0821ba3d780a6fc5e4
Pranav Baburaj <70764593+pranavbaburaj@users.noreply.github.com>
2021-02-13 00:07:46 +0530

    ..

---
+++ scripts/index.js
@@ -0,0 +1,5 @@
+import TypeWriter from "./typing.js"
+
+const text = document.querySelector(".text")
+
+const typeWriterText = 0;
---
+++ scripts/typing.js
@@ -0,0 +1,11 @@
+
+class TypeWriter {
+  constructor(textElement, speed){
+    this.textElement = textElement
+    this.text = textElement.innerHTML
+
+    console.log(this.text)
+  }
+}
+
+export default TypeWriter;
